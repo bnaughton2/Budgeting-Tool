fad4281ad9488539f1be8a61a0d23fcbcaec7d1b
Bobby Naughton <bnaughton22@gmail.com>
2022-02-07 01:27:41 -0500

    Created BudgetingTool folder by creating a Django project and app

---
+++ BudgetingTool/api/views.py
@@ -0,0 +1,7 @@
+from urllib.request import HTTPPasswordMgr
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def main(request):
+    return HttpResponse("<h1>Hello!</h1>")
