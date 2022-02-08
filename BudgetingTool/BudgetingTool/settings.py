be7b053bacb8873a5babd03216a0a5f72862c513
Bobby Naughton <bobbynaughton@Bobbys-MacBook-Air.local>
2022-02-08 15:32:35 -0500

    Added DB configuration and created a file with DB password

--- BudgetingTool/BudgetingTool/settings.py
+++ BudgetingTool/BudgetingTool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pw.py import pw
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'BudgetingTool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Budgeting_Tool_DB',
+        'USER': 'admin',
+        'PASSWORD': pw,
+        'HOST': 'budgeting-tool-db.cd7wgmdfoskl.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
